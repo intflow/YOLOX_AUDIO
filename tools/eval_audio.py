231e7272824f53aeefeeb013318511fddccd102b
kmjeon <kmjeon@intflow.ai>
2021-12-02 11:06:31 +0900

    testing eval_aduio

--- tools/eval_audio.py
+++ tools/eval_audio.py
@@ -6,7 +6,7 @@ from pytictoc import TicToc
 
 def main():
 
-    DATA_PATH = '/data/AIGC_3rd_2021/GIST_tr2_veryhard500/wav'
+    DATA_PATH = '/data/AIGC_3rd_2021/GIST_tr2_1000/wav'
         
     t = TicToc()
     t.tic()
@@ -16,12 +16,12 @@ def main():
     
 
     print("----Write Answer:----")
-    with open("output/tr2_devel_500_est.json", "w", encoding='UTF-8') as json_file:
+    with open("output/tr2_devel_1000.json", "w", encoding='UTF-8') as json_file:
         json.dump(json_out, json_file, indent=2, ensure_ascii=False)
 
     ## 6. Evaluate scores (Target, Estimates)
     import metric_audio
-    metric_audio.load_json('/data/AIGC_3rd_2021/GIST_tr2_veryhard500/tr2_devel_500.json', 'output/tr2_devel_500_est.json')
+    metric_audio.load_json('/data/AIGC_3rd_2021/GIST_tr2_1000/tr2_devel_1000.json', 'output/tr2_devel_1000.json')
     t.toc()
 
 if __name__ == '__main__':
