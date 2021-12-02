5d67eed7ad1ac8ef72c5a4c8a84c95bd148a6c2e
kmjeon <kmjeon@intflow.ai>
2021-12-02 12:02:02 +0900

    checked est file output

--- tools/eval_audio.py
+++ tools/eval_audio.py
@@ -7,6 +7,8 @@ from pytictoc import TicToc
 def main():
 
     DATA_PATH = '/data/AIGC_3rd_2021/GIST_tr2_1000/wav'
+    OUTPUT_PATH = "output/tr2_devel_1000_est.json"
+    ANSWER_PATH = '/data/AIGC_3rd_2021/GIST_tr2_1000/tr2_devel_1000.json'
         
     t = TicToc()
     t.tic()
@@ -16,12 +18,12 @@ def main():
     
 
     print("----Write Answer:----")
-    with open("output/tr2_devel_1000.json", "w", encoding='UTF-8') as json_file:
+    with open(OUTPUT_PATH, "w", encoding='UTF-8') as json_file:
         json.dump(json_out, json_file, indent=2, ensure_ascii=False)
 
     ## 6. Evaluate scores (Target, Estimates)
     import metric_audio
-    metric_audio.load_json('/data/AIGC_3rd_2021/GIST_tr2_1000/tr2_devel_1000.json', 'output/tr2_devel_1000.json')
+    metric_audio.load_json(ANSWER_PATH, OUTPUT_PATH)
     t.toc()
 
 if __name__ == '__main__':
